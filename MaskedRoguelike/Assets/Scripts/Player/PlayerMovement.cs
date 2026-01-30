92201f16dcba69a6c549b95cd4f0eaa69d5d879c
Will Andrew <willandrew3141@gmail.com>
2026-01-30 19:51:55 +0000

    smoothed player movement and added boilerplate for dodging

--- MaskedRoguelike/Assets/Scripts/Player/PlayerMovement.cs
+++ MaskedRoguelike/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private InputActionReference actionRef;
+    private InputActionReference moveRef;
+
+    [SerializeField]
+    private InputActionReference dodgeRef;
 
     [SerializeField]
     private float moveSpeed = 5.0f;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -24,21 +29,32 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveInput.normalized * moveSpeed * Time.fixedDeltaTime);
-
     }
 
     void OnEnable()
     {
-        actionRef.action.Enable();
-        actionRef.action.performed += OnMove;
-        actionRef.action.canceled += OnMoveCanceled;
+        //basic movement
+        moveRef.action.Enable();
+        moveRef.action.performed += OnMove;
+        moveRef.action.canceled += OnMoveCancel;
+
+        //dodging
+        dodgeRef.action.Enable();
+        dodgeRef.action.started += OnDodge;
+        dodgeRef.action.canceled += OnDodgeCancel;
     }
 
     private void OnDisable()
     {
-        actionRef.action.performed -= OnMove;
-        actionRef.action.canceled -= OnMoveCanceled;
-        actionRef.action.Disable(); //disable to prevent ghost inputs
+        //basic movement
+        moveRef.action.performed -= OnMove;
+        moveRef.action.canceled -= OnMoveCancel;
+        moveRef.action.Disable(); //disable to prevent ghost inputs
+
+        //dodging
+        dodgeRef.action.started -= OnDodge;
+        dodgeRef.action.canceled -= OnDodgeCancel;
+        dodgeRef.action.Disable();
     }
 
     private void OnMove(InputAction.CallbackContext context)
@@ -46,8 +62,18 @@ public class PlayerMovement : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
-    private void OnMoveCanceled(InputAction.CallbackContext context)
+    private void OnMoveCancel(InputAction.CallbackContext context)
     {
         moveInput = Vector2.zero;
+    }
+
+    private void OnDodge(InputAction.CallbackContext context)
+    {
+        Debug.Log("Dodging");
+    }
+
+    private void OnDodgeCancel(InputAction.CallbackContext context)
+    {
+        Debug.Log("Stop Dodging");
     }
 }
