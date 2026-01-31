d16949d70de32c06eaf674f0dac817ed941eebbe
Will Andrew <willandrew3141@gmail.com>
2026-01-31 21:52:25 +0000

    started interact logic and UI

---
+++ MaskedRoguelike/Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [Header("Input Action References")]
+    [SerializeField]
+    private InputActionReference interactRef;
+
+    [Header("Interact Variables")]
+
+    public bool canVisitShop = false;
+
+    [Header("Interact UI")]
+    [SerializeField]
+    private Canvas playerCanvas;
+
+    [SerializeField]
+    private Image interactImage;
+
+    private void OnEnable()
+    {
+        interactRef.action.Enable();
+        interactRef.action.started += OnInteract;
+    }
+
+    private void OnDisable()
+    {
+        interactRef.action.started -= OnInteract;
+        interactRef.action.Disable();
+    }
+
+    private void Update()
+    {
+        if (canVisitShop)
+            interactImage.enabled = true;
+        else
+            interactImage.enabled = false;
+    }
+
+    private void OnInteract(InputAction.CallbackContext context)
+    {
+        if(!canVisitShop) { return; }
+
+        Debug.Log("Interact key Pressed");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Merchant")
+            canVisitShop = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Merchant")
+            canVisitShop = false;
+    }
+}
