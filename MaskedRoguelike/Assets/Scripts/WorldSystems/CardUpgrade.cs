6ae50b09ff0b498e4882de9b22aaded147598610
Will Andrew <willandrew3141@gmail.com>
2026-02-01 10:20:38 +0000

    randomising upgrade spite and text

--- MaskedRoguelike/Assets/Scripts/WorldSystems/CardUpgrade.cs
+++ MaskedRoguelike/Assets/Scripts/WorldSystems/CardUpgrade.cs
@@ -11,6 +11,13 @@ public class CardUpgrade : MonoBehaviour
     [SerializeField]
     private string[] cardTexts;
 
+    [Header("Card Components")]
+    [SerializeField]
+    private TextMeshProUGUI cardText;
+
+    [SerializeField]
+    private Image cardSprite;
+
     public enum CardTypes
     {
         None,
@@ -22,12 +29,44 @@ public class CardUpgrade : MonoBehaviour
         BurnDuration
     }
 
-    [Header("Card Components")]
-    [SerializeField]
-    private TextMeshProUGUI cardText;
+    private void Awake()
+    {
+        cardSprite = GetComponent<Image>();
+        cardText = GetComponentInChildren<TextMeshProUGUI>();
+    }
 
     public void RandomiseUpgrade()
     {
-        Debug.Log(gameObject.name + "Randomising Upgrades");
+        CardTypes cardType = (CardTypes)Random.Range(1, 6);
+
+        print("card " + gameObject.name + " : " + cardType);
+
+        switch(cardType)
+        {
+            case CardTypes.FrostStrength:
+                cardSprite.sprite = cardSprites[0];
+                cardText.text = cardTexts[0];
+                break;
+            case CardTypes.FrostDuration:
+                cardSprite.sprite = cardSprites[0];
+                cardText.text = cardTexts[1];
+                break;
+            case CardTypes.Heal:
+                cardSprite.sprite = cardSprites[1];
+                cardText.text = cardTexts[2];
+                break;
+            case CardTypes.MaxHealth:
+                cardSprite.sprite = cardSprites[1];
+                cardText.text = cardTexts[3];
+                break;
+            case CardTypes.BurnDamage:
+                cardSprite.sprite = cardSprites[2];
+                cardText.text = cardTexts[4];
+                break;
+            case CardTypes.BurnDuration:
+                cardSprite.sprite = cardSprites[2];
+                cardText.text = cardTexts[5];
+                break;
+        }
     }
 }
